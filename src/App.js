953a68c04a0add95399b72e47f6cebbdb756a798
educprofissoes <educprofissoes@gmail.com>
2024-06-12 16:36:20 -0300

    Update files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home'
 import About from './pages/About';
@@ -12,16 +12,18 @@ import WorkWithUs from './pages/WorkWithUs';
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vocational" element={<Vocational />} />
-        <Route path="/#/technical" element={<Technical />} />
-        <Route path="/#/degree" element={<Degree />} />
-        <Route path="/#/language" element={<Language />} />
-        <Route path="/#/vocational/:id" element={<Details />} />
-        <Route path="/#/work-with-us" element={<WorkWithUs />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vocational" element={<Vocational />} />
+          <Route path="/#/technical" element={<Technical />} />
+          <Route path="/#/degree" element={<Degree />} />
+          <Route path="/#/language" element={<Language />} />
+          <Route path="/#/vocational/:id" element={<Details />} />
+          <Route path="/#/work-with-us" element={<WorkWithUs />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
