a839a5c97dc5319147f148b6d86c29705211f3ab
educprofissoes <educprofissoes@gmail.com>
2024-06-29 12:59:13 -0300

    Update Files

--- src/db/courses.js
+++ src/db/courses.js
@@ -1,7 +1,7 @@
 export const db = [
   {
     id: 1,
-    title: 'Curso de Montagem e Manutenção de Computadores e Redes',
+    title: 'Montagem e Manutenção de Computadores e Redes',
     description: 'O curso de Manutenção e Montagem se destina à pessoas que pretendem trabalhar na área ou para aqueles que pretendem expandir seu conhecimento acerca do funcionamento de um computador. O curso abrange uma ampla gama de temas essenciais para otimizar o desempenho e resolver problemas comuns de computadores. Os alunos aprendem sobre a arquitetura de computadores, com ênfase nos processadores de última geração da Intel e AMD, incluindo os de múltiplos núcleos. Eles são guiados na montagem correta do processador e do sistema de refrigeração, além de aprenderem sobre memória, armazenamento, placa-mãe e configuração de placa de vídeo e sistema operacional. Por fim, são apresentados conceitos e técnicas para montagem de redes locais (LAN).',
     hours: '46 horas',
     requirements: 'Ter concluído o curso de Informática ou possuir conhecimentos equivalentes.',
@@ -12,7 +12,7 @@ export const db = [
   },
   {
     id: 2,
-    title: 'Curso de Design Gráfico',
+    title: 'Design Gráfico',
     description: 'O curso de Designer Gráfico é um curso que se destina à todas as pessoas independentemente da idade ou profissão e este curso abordará os fundamentos essenciais do design gráfico, com foco na utilização eficaz das ferramentas Adobe Photoshop, Illustrator, InDesign e CorelDRAW. Os participantes serão guiados através de técnicas fundamentais de design, incluindo composição, tipografia, uso de cores e manipulação de imagens. Além disso, serão exploradas as capacidades específicas de cada software, desde a edição avançada de fotos no Photoshop até a criação de ilustrações vetoriais no Illustrator e CorelDRAW, e o layout de publicações no InDesign.',
     hours: '93 horas',
     requirements: 'Ter concluído o curso de Informática ou possuir conhecimentos equivalentes.',
@@ -23,7 +23,7 @@ export const db = [
   },
   {
     id: 3,
-    title: 'Curso de Youtubers',
+    title: 'Youtubers',
     description: 'O curso de Youtubers é um curso que se destina à todas as pessoas independentemente da idade ou profissão e que reúne alguns assuntos que avaliamos como importantes famosa da internet, o Youtube. Ele surge como um complemento e com a função de sanar as principais dúvidas que surgem ao aluno, que são: Como faço para criar uma conta no Youtube? Como público meus próprios vídeos no Youtube? É possível ganhar dinheiro publicando vídeos na internet? Como faço uma transmissão ao vivo no Youtube?',
     hours: '127 horas',
     requirements: 'Ter concluído o curso de Informática ou possuir conhecimentos equivalentes.',
@@ -34,7 +34,7 @@ export const db = [
   },
   {
     id: 4,
-    title: 'Curso de Excel Avançado',
+    title: 'Excel Avançado',
     description: 'O Curso de Microsoft Excel Avançado é dirigido a Pessoas de qualquer área de atuação como Finanças, Estatística, Contabilidade, Vendas, Engenharia etc, que já conhece o Excel e tenha necessidades em automatizar planilhas fim de deixá-las mais eficientes e até mesmo desenvolver aplicações no Excel.',
     hours: '24 horas',
     requirements: 'Ter concluído o curso Excel Básico ou possuir conhecimentos equivalentes.',
@@ -42,5 +42,82 @@ export const db = [
     objective: 'Capacitar o aluno a realizar cálculos utilizando fórmulas e funções, analisar dados, utilizar o recurso de tabela dinâmica, introdução a macro, além de trabalhar com ferramentas que possibilitem ganho de produtividade e trabalho colaborativo com outros usuários.',
     preco: 149.99,
     imagemUrl: 'https://www.gov.br/transportes/pt-br/assuntos/portal-da-estrategia/cursos-e-eventos-estrategia/excel-avancado-2/excel-avancado-jpg/@@images/6687d1d2-820f-4b1b-b7d5-2c6cc7015daf.jpeg'
-  }
+  },
+  {
+    id: 5,
+    title: 'AutoCAD',
+    description: 'O curso de AutoCAD, destina-se a todo aquele que deseja trabalhar com desenhos técnicos ou aqueles que já trabalham na área e ainda estão no método antigo de desenho (prancheta) ou que desejam trabalhar com modelagem de projetos 3D.',
+    hours: '65 horas',
+    requirements: 'O aluno já deve ter conhecimento em informática. É indispensável que o aluno se interesse por desenhos técnicos ou já os conheça.',
+    techniques: 'Simulador e apostila.',
+    objective: 'Os curso de AutoCAD destaca a importância de aprender e dominar as ferramentas necessárias para criar e editar desenhos técnicos, tanto em 2D quanto em 3D. Ao adquirir esse conhecimento, os alunos estarão bem preparados para desenvolver projetos em suas respectivas áreas profissionais, abrangendo tanto o design bidimensional quanto o tridimensional.',
+    preco: 149.99,
+    imagemUrl: 'https://escolamegacursos.com/painel/arquivos/imagens/autocad.png'
+  },
+  {
+    id: 6,
+    title: 'Desenvolvimento de Jogos',
+    description: 'O curso de Desenvolvimento de Jogos é indicado a todas as pessoas que queiram adquirir conhecimento de um desenvolvedor de jogos utilizando as principais plataformas do mercado.',
+    hours: '100 horas',
+    requirements: 'O aluno já deve ter conhecimento em informática e Windows.',
+    techniques: 'Simulador e apostila.',
+    objective: 'O curso visa reparar o aluno para usufruir com facilidade das ferramentas e recursos das plataformas Unity, Unreal e Cry, além de desenvolver criatividade e domínio de todos os processos da criação de um jogo.',
+    preco: 149.99,
+    imagemUrl: 'https://cgicoffee.com/blog/content/images/2018/27jan2018/game-engines.png'
+  },
+  {
+    id: 7,
+    title: 'Desenvolvimento WEB',
+    description: 'O curso de Desenvolvimento WEB é indicado a todas as pessoas que queiram adquirir conhecimento de um desenvolvedor aplicações WEB utilizando ferramentas como HTML, CSS e JavaScript.',
+    hours: '100 horas',
+    requirements: 'O aluno já deve ter conhecimento em informática e Windows.',
+    techniques: 'Simulador e apostila.',
+    objective: 'O curso tem o objetivo de transmitir conhecimentos fundamentais em HTML, CSS e JavaScript, proporcionando conhecimentos essenciais para criar e estilizar páginas web interativas. Ao final do curso, os alunos serão capazes de desenvolver páginas web simples e dinâmicas, utilizando técnicas básicas de programação e design front-end.',
+    preco: 149.99,
+    imagemUrl: 'https://apexensino.com.br/wp-content/uploads/2017/11/html-css-javascript.jpg'
+  },
+  {
+    id: 8,
+    title: 'Robótica',
+    description: 'O curso de Robótica é destinado a estudantes e profissionais interessados em adquirir habilidades práticas em eletrônica, programação e uso do Arduino para projetos robóticos. É ideal para quem busca explorar desde conceitos básicos até aplicações avançadas de automação e inovação tecnológica na área de robótica.',
+    hours: '100 horas',
+    requirements: 'O aluno já deve ter conhecimento em informática e Windows.',
+    techniques: 'Simulador e apostila.',
+    objective: 'Este curso tem como objetivo capacitar os participantes a compreender os princípios fundamentais da eletricidade e eletrônica aplicados à robótica, além de proporcionar habilidades avançadas na utilização e programação de plataformas como Arduino. Ao final do curso, os alunos estarão aptos a projetar, construir e programar robôs autônomos e sistemas automatizados, explorando conceitos teóricos e práticos essenciais para a área.',
+    preco: 149.99,
+    imagemUrl: 'https://miro.medium.com/v2/resize:fit:1400/1*0efkEmnD0_DWYYGmKeqpsg.jpeg'
+  },
+  {
+    id: 9,
+    title: 'Informática',
+    description: 'O curso de Informática abrange os módulos de Introdução à Informática, Internet, Windows e Pacote Office, sendo direcionado a iniciantes e profissionais que desejam desenvolver competências essenciais em tecnologia. Ideal para quem busca aprender desde conceitos fundamentais de informática até o uso prático da Internet, operação do sistema Windows e aplicativos do Pacote Office.',
+    hours: '100 horas',
+    requirements: 'Não existem pré-requisitos para este curso.',
+    techniques: 'Simulador e apostila.',
+    objective: 'Capacitar os participantes a dominar os fundamentos da informática, incluindo o uso eficaz da Internet, operação do sistema Windows e aplicativos do Pacote Office. Ao final do curso, os alunos serão capazes de utilizar recursos básicos de computação de forma eficiente, realizar tarefas comuns de escritório e navegar na web com segurança e confiança.',
+    preco: 149.99,
+    imagemUrl: 'https://cfis.com.br/site/wp-content/uploads/2022/06/imagem-informatica-basica-e-avancada.png'
+  },
+  {
+    id: 10,
+    title: 'Rotinas Administrativas',
+    description: 'O curso de Rotinas Administrativas é voltado para profissionais e estudantes que buscam adquirir conhecimentos abrangentes na área administrativa. Ideal para aqueles interessados em desenvolver habilidades em atendimento ao cliente, gestão empresarial, contabilidade básica, administração de pessoal, logística e técnicas eficazes de vendas.',
+    hours: '100 horas',
+    requirements: 'Não existem pré-requisitos para este curso.',
+    techniques: 'Simulador e apostila.',
+    objective: 'Preparar os participantes para atuarem de forma competente e eficiente em diversas áreas da administração empresarial. Ao final do curso, os alunos estarão capacitados a aplicar técnicas avançadas em telemarketing, criar e gerenciar empresas, executar atividades de secretariado, realizar procedimentos básicos de contabilidade e departamento pessoal, além de dominar conceitos essenciais de gestão de pessoas, logística empresarial e técnicas estratégicas de venda.',
+    preco: 149.99,
+    imagemUrl: 'https://portalidea.com.br/cursos/rotinas-administrativas.webp'
+  },
+  {
+    id: 11,
+    title: 'Pacote Office',
+    description: 'Este curso destina-se a estudantes, profissionais e qualquer pessoa interessada em adquirir habilidades avançadas no uso do Pacote Office. É ideal para aqueles que buscam aprimorar sua produtividade no ambiente de trabalho, dominando as ferramentas essenciais como Word para criação de documentos, Excel para análise e organização de dados, e PowerPoint para criação de apresentações impactantes. Também é recomendado para indivíduos que desejam melhorar suas competências digitais e aumentar suas oportunidades no mercado de trabalho.',
+    hours: '60 horas',
+    requirements: 'O aluno já deve ter conhecimento em informática e Windows.',
+    techniques: 'Simulador e apostila.',
+    objective: 'Capacitar os participantes a dominar as principais ferramentas do Pacote Office, incluindo Word para criação e formatação de documentos, Excel para análise e manipulação de dados, e PowerPoint para criação de apresentações dinâmicas e profissionais. Ao final do curso, os alunos serão proficientes no uso dessas aplicações, preparando-os para realizar tarefas complexas de escritório e apresentações impactantes no ambiente profissional.',
+    preco: 149.99,
+    imagemUrl: 'https://www.primecursos.com.br/blog/wp-content/uploads/2019/11/curso-de-pacote-office-4.png'
+  },
 ]
